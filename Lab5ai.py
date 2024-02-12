4fa4b913c16233f1130efe936753b0dbaaa01f35
mjide-ogungbe <156146122+mjide-ogungbe@users.noreply.github.com>
2024-02-12 14:34:06 -0500

    Add files via upload

---
+++ Lab5ai.py
@@ -0,0 +1,10 @@
+#This function calculates your present age  
+
+# using the define function 'def' 
+def calculate_age():
+    user_age =input('What is your year of birth: ')
+    age = 2024 - int(user_age)
+    return age 
+
+
+print(calculate_age())
